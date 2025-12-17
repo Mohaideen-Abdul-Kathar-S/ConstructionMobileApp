09eefd95e9107ec767f702380ca83bb58ab418a2
Mohaideen Abdul Kathar S <mohaideenabdulkathars@gmail.com>
2025-12-17 08:09:37 +0530

    Remove backend server from frontend branch

--- Server/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-    Username: String,
-    Password: String,
-    Email: String,
-});
-
-export default mongoose.model('Users', userSchema);
